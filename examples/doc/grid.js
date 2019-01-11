2492cf1c7033db435aee68167f32fb03990c3e71
itobys <1378993548@qq.com>
2019-01-11 13:52:13 +0800

    v0.1.7 新增完整grid布局案例&文档，优化grid样式

--- examples/doc/grid.js
+++ examples/doc/grid.js
@@ -20,21 +20,211 @@ code.BASE = `
 </template>
 `;
 
+code.GUTTER = `
+<template>
+  <c-row gutter="18" class="gutter">
+    <c-col span="6"><div>col-span-6</div> </c-col>
+    <c-col span="6"><div>col-span-6</div> </c-col>
+    <c-col span="6"><div>col-span-6</div> </c-col>
+    <c-col span="6"><div>col-span-6</div> </c-col>
+  </c-row>
+</template>
+`;
+
+code.ORDER = `
+<template>
+  <c-row flex>
+    <c-col span="6" order="1">4 | order-1</c-col>
+    <c-col span="6" order="3">2 | order-3</c-col>
+    <c-col span="6" order="2">3 | order-2</c-col>
+    <c-col span="6" order="4">1 | order-4</c-col>
+  </c-row>
+</template>
+`;
+
+code.PUSHPULL = `
+<template>
+  <c-row>
+    <c-col push="6" span="18">col-18 | col-pull-6</c-col>
+    <c-col pull="18" span="6">col-6 | col-pull-18</c-col>
+  </c-row>
+</template>
+`;
+
+code.OFFSET = `
+<template>
+  <c-row>
+    <c-col span="8">col-8 </c-col>
+    <c-col span="8" offset="8">col-8 | col-offset-8</c-col>
+  </c-row>
+  <c-row>
+    <c-col span="6" offset="6">col-8 | col-offset-8</c-col>
+    <c-col span="6" offset="6">col-8 | col-offset-8</c-col>
+  </c-row>
+  <c-row>
+    <c-col span="8" offset="12">col-8 | col-offset-12</c-col>
+  </c-row>
+</template>
+`;
+
+code.FLEXBJ = `
+<template>
+  <p class="description">子元素向左排列</p>
+  <c-row flex justify="start" class="code-row-bg">
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+  </c-row>
+  <p class="description">子元素向右排列</p>
+  <c-row flex justify="end" class="code-row-bg">
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+  </c-row>
+  <p class="description">子元素居中排列</p>
+  <c-row flex justify="center" class="code-row-bg">
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+  </c-row>
+  <p class="description">子元素等宽排列</p>
+  <c-row flex justify="space-between" class="code-row-bg">
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+  </c-row>
+  <p class="description">子元素分散排列</p>
+  <c-row flex justify="space-around" class="code-row-bg">
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+    <c-col span="4"> col-4 </c-col>
+  </c-row>
+</template>
+`;
+
+code.FLEXDQ = `
+<template>
+  <p class="description">顶部对齐</p>
+  <c-row flex justify="center" align="top" class="code-row-bg">
+    <c-col span="4" style="line-height: 80px;">col-4</c-col>
+    <c-col span="4" style="line-height: 40px;">col-4</c-col>
+    <c-col span="4" style="line-height: 100px">col-4</c-col>
+    <c-col span="4" style="line-height: 60px">col-4</c-col>
+  </c-row>
+  <p class="description">居中对齐</p>
+  <c-row flex justify="center" align="middle" class="code-row-bg">
+    <c-col span="4" style="line-height: 80px;">col-4</c-col>
+    <c-col span="4" style="line-height: 40px;">col-4</c-col>
+    <c-col span="4" style="line-height: 100px">col-4</c-col>
+    <c-col span="4" style="line-height: 60px">col-4</c-col>
+  </c-row>
+  <p class="description">底部对齐</p>
+  <c-row flex justify="center" align="bottom" class="code-row-bg">
+    <c-col span="4" style="line-height: 80px;">col-4</c-col>
+    <c-col span="4" style="line-height: 40px;">col-4</c-col>
+    <c-col span="4" style="line-height: 100px">col-4</c-col>
+    <c-col span="4" style="line-height: 60px">col-4</c-col>
+  </c-row>
+</template>
+`;
+
+code.XYSBJ = `
+<template>
+  <c-row>
+    <c-col :xs="2" :sm="4" :md="6" :lg="8">Col</c-col>
+    <c-col :xs="20" :sm="16" :md="12" :lg="8">Col</c-col>
+    <c-col :xs="2" :sm="4" :md="6" :lg="8">Col</c-col>
+  </c-row>
+</template>
+`;
+
 code.CRowDoc = [
   {
-    type: '',
-    desc: '',
-    typeof: '',
-    default: ""
+    type: "gutter",
+    desc: `栅格间距，单位<code>px</code>，左右平分`,
+    typeof: "String | Number",
+    default: "0"
+  },
+  {
+    type: "flex",
+    desc: `布局模式, 在<code>CRow</code>直接传入即可开启`,
+    typeof: "Boolean",
+    default: "false"
+  },
+  {
+    type: "justify",
+    desc: `flex布局的水平排列方式, 可选值<code>start</code>、<code>end</code>、<code>center</code>、<code>space-around</code>、<code>space-between</code>`,
+    typeof: "String",
+    default: "-"
+  },
+  {
+    type: "align",
+    desc:
+      "flex布局的垂直居中方式, 可选值<code>top</code>、<code>bottom</code>、<code>middle</code>",
+    typeof: "String",
+    default: "-"
   }
 ];
 
 code.CColDoc = [
   {
-    type: '',
-    desc: '',
-    typeof: '',
-    default: ""
+    type: "span",
+    desc: `栅格的占位格数，可选值为<code>0~24</code>的整数`,
+    typeof: "String | Number",
+    default: "-"
+  },
+  {
+    type: "order",
+    desc: `栅格顺序, 在<code>flex</code>模式下有效`,
+    typeof: "String | Number",
+    default: "-"
+  },
+  {
+    type: "offset",
+    desc: "栅格左侧的间隔格数, 间隔内不可以有栅格",
+    typeof: "String | Number",
+    default: "-"
+  },
+  {
+    type: "push",
+    desc: "栅格向右移动格数",
+    typeof: "String | Number",
+    default: "-"
+  },
+  {
+    type: "pull",
+    desc: "栅格向左移动格数",
+    typeof: "String | Number",
+    default: "-"
+  },
+  {
+    type: "xs",
+    desc: `<code><768px</code> 响应式栅格`,
+    typeof: "String | Number",
+    default: "-"
+  },
+  {
+    type: "sm",
+    desc: `<code>≥768px</code> 响应式栅格`,
+    typeof: "String | Number",
+    default: "-"
+  },
+  {
+    type: "md",
+    desc: `<code>≥992px</code> 响应式栅格`,
+    typeof: "String | Number",
+    default: "-"
+  },
+  {
+    type: "lg",
+    desc: `<code>≥1200px</code> 响应式栅格`,
+    typeof: "String | Number",
+    default: "-"
   }
 ];
 
--- examples/view/components/grid.vue
+++ examples/view/components/grid.vue
@@ -65,7 +65,7 @@
       <!-- 区块间距 -->
       <Demo id="GUTTER">
         <div slot="case">
-          <c-row gutter="18" class="gutter">
+          <c-row :gutter="18" class="gutter">
             <c-col span="6"><div>col-span-6</div> </c-col>
             <c-col span="6"><div>col-span-6</div> </c-col>
             <c-col span="6"><div>col-span-6</div> </c-col>
@@ -76,7 +76,154 @@
         <div slot="desc">
           <p>通过给 <code>CRow</code> 添加 <code>gutter</code> 属性, 可以给下属的 <code>col</code> 作为栅格间隔。</p>
         </div>
+        <code-demo lang="html" slot="code" >{{ GridDoc.GUTTER }}</code-demo>
       </Demo>
+      <!-- flex布局排序 -->
+      <Demo id="FLEXORDER">
+        <div slot="case">
+          <c-row flex>
+            <c-col span="6" order="1">4 | order-1</c-col>
+            <c-col span="6" order="3">2 | order-3</c-col>
+            <c-col span="6" order="2">3 | order-2</c-col>
+            <c-col span="6" order="4">1 | order-4</c-col>
+          </c-row>
+        </div>
+        <span slot="title" class="title">栅格排序(flex布局)</span>
+        <div slot="desc">
+          <p>通过<code>Flex</code>布局的<code>order</code>属性, 可对区块进行排序</p>
+        </div>
+        <code-demo lang="html" slot="code" >{{ GridDoc.ORDER }}</code-demo>
+      </Demo>
+      <!--左&右偏移-->
+      <Demo id="PUSHPULL">
+        <div slot="case">
+          <c-row>
+            <c-col push="6" span="18">col-18 | col-pull-6</c-col>
+            <c-col pull="18" span="6">col-6 | col-pull-18</c-col>
+          </c-row>
+        </div>
+        <span slot="title" class="title">栅格排序</span>
+        <div slot="desc">
+          <p>通过设置<code>push</code>和<code>pull</code>属性来改变栅格的排序</p>
+        </div>
+        <code-demo lang="html" slot="doc"> {{ GridDoc.PUSHPULL }}</code-demo>
+      </Demo>
+      <!-- 偏移 -->
+      <Demo id="OFFSET">
+        <div slot="case">
+          <c-row>
+            <c-col span="8">col-8 </c-col>
+            <c-col span="8" offset="8">col-8 | col-offset-8</c-col>
+          </c-row>
+          <c-row>
+            <c-col span="6" offset="6">col-8 | col-offset-8</c-col>
+            <c-col span="6" offset="6">col-8 | col-offset-8</c-col>
+          </c-row>
+          <c-row>
+            <c-col span="8" offset="12">col-8 | col-offset-12</c-col>
+          </c-row>
+        </div>
+        <span slot="title" class="title">栅格偏移(左偏移)</span>
+        <div slot="desc">
+          <p>通过设置<code>offset</code>属性, 可将<code>CCol</code>进行左偏移, 偏移栅格数尽可能与<code>span</code>的值一致, 不得超过24</p>
+        </div>
+        <code-demo lang="html" slot="doc">{{ GridDoc.OFFSET }}</code-demo>
+      </Demo>
+      <!-- 布局 -->
+      <Demo id="FLEXBJ">
+        <div slot="case">
+          <p class="description">子元素向左排列</p>
+          <c-row flex justify="start" class="code-row-bg">
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+          </c-row>
+          <p class="description">子元素向右排列</p>
+          <c-row flex justify="end" class="code-row-bg">
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+          </c-row>
+          <p class="description">子元素居中排列</p>
+          <c-row flex justify="center" class="code-row-bg">
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+          </c-row>
+          <p class="description">子元素等宽排列</p>
+          <c-row flex justify="space-between" class="code-row-bg">
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+          </c-row>
+          <p class="description">子元素分散排列</p>
+          <c-row flex justify="space-around" class="code-row-bg">
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+            <c-col span="4"> col-4 </c-col>
+          </c-row>
+        </div>
+        <span slot="title" class="title">Flex布局</span>
+        <div slot="desc">
+          <p>通过设置<code>justify</code>属性改变子元素的排布方式, 只在<code>flex</code>下有效</p>
+        </div>
+        <code-demo lang="html" slot="code"> {{ GridDoc.FLEXBJ }} </code-demo>
+      </Demo>
+      <!-- 对齐 -->
+      <Demo id="FLEXDQ">
+        <div slot="case">
+          <p class="description">顶部对齐</p>
+          <c-row flex justify="center" align="top" class="code-row-bg">
+            <c-col span="4" style="line-height: 80px;">col-4</c-col>
+            <c-col span="4" style="line-height: 40px;">col-4</c-col>
+            <c-col span="4" style="line-height: 100px">col-4</c-col>
+            <c-col span="4" style="line-height: 60px">col-4</c-col>
+          </c-row>
+          <p class="description">居中对齐</p>
+          <c-row flex justify="center" align="middle" class="code-row-bg">
+            <c-col span="4" style="line-height: 80px;">col-4</c-col>
+            <c-col span="4" style="line-height: 40px;">col-4</c-col>
+            <c-col span="4" style="line-height: 100px">col-4</c-col>
+            <c-col span="4" style="line-height: 60px">col-4</c-col>
+          </c-row>
+          <p class="description">底部对齐</p>
+          <c-row flex justify="center" align="bottom" class="code-row-bg">
+            <c-col span="4" style="line-height: 80px;">col-4</c-col>
+            <c-col span="4" style="line-height: 40px;">col-4</c-col>
+            <c-col span="4" style="line-height: 100px">col-4</c-col>
+            <c-col span="4" style="line-height: 60px">col-4</c-col>
+          </c-row>
+        </div>
+        <span class="title" slot="title">Flex对齐</span>
+        <div slot="desc">
+          <p>通过设置<code>align</code>属性改变子元素的对齐位置, 只在<code>flex</code>下有效</p>
+        </div>
+        <code-demo lang="html" slot="code"> {{ GridDoc.FLEXDQ }} </code-demo>
+      </Demo>
+      <!--响应式布局-->
+      <Demo id="XYSBJ">
+        <div slot="case">
+          <c-row>
+            <c-col :xs="2" :sm="4" :md="6" :lg="8">Col</c-col>
+            <c-col :xs="20" :sm="16" :md="12" :lg="8">Col</c-col>
+            <c-col :xs="2" :sm="4" :md="6" :lg="8">Col</c-col>
+          </c-row>
+        </div>
+        <span slot="title" class="title">响应式布局</span>
+        <div slot="desc">
+          <p>参照<code>Bootstrap</code>的响应式设计, 提供四个响应式尺寸<code>xs</code>、<code>sm</code>、<code>md</code>、<code>lg</code>
+            详情请看API
+          </p>
+        </div>
+        <code-demo lang="html" slot="code">{{ GridDoc.XYSBJ }}</code-demo>
+      </Demo>
+      <api-table title="API" sub-title="Row props" :doc="GridDoc.CRowDoc"></api-table>
+      <api-table sub-title="CCol props" :doc="GridDoc.CColDoc"></api-table>
     </article>
   </c-article>
 </template>
@@ -105,6 +252,9 @@ export default {
 </script>
 
 <style lang="scss">
+.code-row-bg {
+  background: rgba(0,0,0,.1);
+}
 .demo-row {
   margin-bottom: 5px;
   .demo-col {
